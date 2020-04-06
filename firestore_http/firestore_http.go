c6d548a2c9ec2e4969ba0c2387d8a67d09f860bd
Jonatan Mc Evenue <jonatan@littleglitch.com>
2020-04-06 22:46:39 +0200

    Refactored and removed dependancy on schema module

--- firestore_http/firestore_http.go
+++ firestore_http/firestore_http.go
@@ -7,54 +7,47 @@ import (
 	"regexp"
 
 	"cloud.google.com/go/firestore"
-	"github.com/gorilla/schema"
 )
 
-// Add is an HTTP Cloud Function with a request parameter.
+// Add is an HTTP Cloud Function with a URL parameter.
 func Add(w http.ResponseWriter, r *http.Request) {
-	var q struct {
-		ID string `json:"id"`
-	}
 
-	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	filter, err := regexp.Compile("[^a-zA-Z0-9]+")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	var decoder = schema.NewDecoder()
-	err = decoder.Decode(&q, r.URL.Query())
+	keys, ok := r.URL.Query()["id"]
+	if !ok || len(keys[0]) < 1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	value := filter.ReplaceAllString(keys[0], "")
+
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, os.Getenv("GCLOUD_PROJECT"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer client.Close()
+
+	_, _, err = client.Collection(os.Getenv("GCLOUD_DATABASE_COLLECTION")).Add(ctx, map[string]interface{}{
+		"timestamp": firestore.ServerTimestamp,
+		"id":        value,
+	})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	if q.ID != "" {
-		ctx := context.Background()
-		client, err := firestore.NewClient(ctx, os.Getenv("GCLOUD_PROJECT"))
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		defer client.Close()
-
-		_, _, err = client.Collection(os.Getenv("GCLOUD_DATABASE_COLLECTION")).Add(ctx, map[string]interface{}{
-			"timestamp": firestore.ServerTimestamp,
-			"id":        reg.ReplaceAllString(q.ID, ""),
-		})
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("OK"))
-
-	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Thank you for your submission\n"))
 
 	return
 }
