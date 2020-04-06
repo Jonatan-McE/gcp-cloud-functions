4043da152e575be41178d7951d4e5a873686fc30
Jonatan Mc Evenue <jonatan@littleglitch.com>
2020-04-06 22:19:45 +0200

    Fixed extra database entry

--- firestore_http/firestore_http.go
+++ firestore_http/firestore_http.go
@@ -15,12 +15,14 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	var q struct {
 		ID string `json:"id"`
 	}
+
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
 	}
+
 	var decoder = schema.NewDecoder()
 	err = decoder.Decode(&q, r.URL.Query())
 	if err != nil {
@@ -29,26 +31,30 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ctx := context.Background()
-	client, err := firestore.NewClient(ctx, os.Getenv("GCLOUD_PROJECT"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	defer client.Close()
+	if q.ID != "" {
+		ctx := context.Background()
+		client, err := firestore.NewClient(ctx, os.Getenv("GCLOUD_PROJECT"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer client.Close()
 
-	_, _, err = client.Collection(os.Getenv("GCLOUD_DATABASE_COLLECTION")).Add(ctx, map[string]interface{}{
-		"timestamp": firestore.ServerTimestamp,
-		"id":        reg.ReplaceAllString(q.ID, ""),
-	})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
+		_, _, err = client.Collection(os.Getenv("GCLOUD_DATABASE_COLLECTION")).Add(ctx, map[string]interface{}{
+			"timestamp": firestore.ServerTimestamp,
+			"id":        reg.ReplaceAllString(q.ID, ""),
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("OK"))
+
 	}
 
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("OK"))
 	return
 }
